133c999d0fec7929d30ca6f6c41cab95fadfe96b
Federico Capoano <nemesis@ninux.org>
2016-03-04 19:19:46 +0100

    Improved install-dev.sh script #16

--- install-dev.sh
+++ install-dev.sh
@@ -5,19 +5,23 @@ export CFLAGS="-Wno-error"
 apt-get update
 # install cmake and git
 apt-get install -y cmake git-core
-# install json-c and lua
-apt-get install -y libjson0 libjson0-dev lua5.1 liblua5.1-0-dev
+# install lua
+apt-get install -y lua5.1 liblua5.1-0-dev
+# install json-c
+apt-get install -y dh-autoreconf
+git clone https://github.com/json-c/json-c.git --depth=1 && cd json-c
+sh autogen.sh && ./configure && make && make install && cd ..
 # install openwrt libubox and uci
 git clone https://git.openwrt.org/project/libubox.git --depth=1 && cd libubox
-cmake . && sudo make install && cd ..
+cmake . && make install && cd ..
 git clone https://git.openwrt.org/project/uci.git --depth=1 && cd uci
-cmake . && sudo make install && cd ..
+cmake . && make install && cd ..
 # update links to shared libraries
-sudo ldconfig -v
+ldconfig -v
 # install luaunit
 git clone https://github.com/bluebird75/luaunit.git --depth=1
 mkdir -p /usr/share/lua/5.1/
-cd luaunit && sudo cp luaunit.lua /usr/share/lua/5.1/ && cd ..
+cd luaunit && cp luaunit.lua /usr/share/lua/5.1/ && cd ..
 test -f /usr/share/lua/5.1/luaunit.lua
 # clean
-rm -rf libubox uci luaunit
+rm -rf json-c libubox uci luaunit
