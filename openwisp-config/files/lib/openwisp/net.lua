df4df5d00e6098486ac948b9139a820e300524c7
Patrick Grimm <patrick@lunatiki.de>
2025-01-30 23:52:58 +0100

    add uci config family (inet|inet6) and ula add comments

--- openwisp-config/files/lib/openwisp/net.lua
+++ openwisp-config/files/lib/openwisp/net.lua
@@ -15,16 +15,19 @@ function net.get_interface(name, family, ula)
     -- default to supplied name if none is found
     ifname = uci_cursor:get('network', name, 'ifname') or name
   end
-  -- get list of interfaces and loop until found
+  -- get list of interfaces
   local interfaces = nixio.getifaddrs()
   -- sort list of interfaces by addr (2001:xx:xx:xx::1,fd52:xx:xx::1,fe80::xx:xx:xx:901a)
   table.sort(interfaces, function (left, right)
     return left['addr'] < right['addr']
   end)
+  -- loop until found
   for _, interface in pairs(interfaces) do
     if interface.name == ifname and interface.family == ip_family then
       if ip_family == 'inet6' then
+        -- check if limklocal fe80::xx:xx:xx:xx addr
         if string.find(interface.addr,'fe') ~= 1 then
+          -- check if ula fd52:xx:xx::1 addr
           if not ula and string.find(interface.addr,'fd') ~= 1 then
             return interface
           elseif ula then
--- openwisp-config/files/sbin/openwisp-get-address.lua
+++ openwisp-config/files/sbin/openwisp-get-address.lua
@@ -2,13 +2,15 @@
 
 -- gets the first useful address of the specified interface
 -- usage:
---   * openwisp-get-address <ifname>
---   * openwisp-get-address <network-name>
+--   * openwisp-get-address <ifname> ["inet"|"inet6"] [ula 1|0]
+--   * openwisp-get-address <network-name> ["inet"|"inet6"] [ula 1|0]
 local os = require('os')
 local net = require('openwisp.net')
 local name = arg[1]
-local interface = net.get_interface(name)
-if not interface then
+local family = arg[2]
+local ula = arg[3]
+local interface = net.get_interface(name, family, ula)
+if not interface and not family then
   interface = net.get_interface(name, 'inet')
 end
 
